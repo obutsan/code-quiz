ce12827547abb17b7ec7153d673151885f6016cb
obutsan <olha.pobedynska@gmail.com>
2025-01-10 12:09:56 +0000

    edit styles challange1

--- src/components/JavaScriptChallenge/JavaScriptChallenge.jsx
+++ src/components/JavaScriptChallenge/JavaScriptChallenge.jsx
@@ -7,37 +7,31 @@ export default function JavaScriptChallenge({ count, handleAnswerButton, isFormD
 	const answerOptions = questions[count].options || [];
 
 	return (
-		<div className="container challenge1">
-			<div className="d-block mx-lg-auto img-fluid px-2">
-        <img className="hero1 object-fit-contain d-block justify-content-center " src="images/officeKnight.gif" alt="hero1" style={{maxWidth: '20em', height: '100%'}}></img>
+	  <div className="container container-fluid  challenge1 d-flex flex-column flex-lg-row align-items-center justify-content-center gap-3 py-3"
+      style={{ backgroundColor: '#0d213c'}} id='challengeLevel1'>
+
+      <div className="col-12 col-md-6 text-center">
+        <img className="hero1 img-fluid" src="images/officeKnight.gif" alt="hero1" style={{ maxWidth: '20em', height: 'auto' }}/>
       </div>
-			<div className='container challenge' id='challengeLevel1'>
-				<h3 className='question'>{question}</h3>
-				<div className='options'>
-					<form onSubmit={handleAnswerButton} disabled={isFormDisabled}>
-						{answerOptions.map((option, index) => (
-							<div key={uuidv4()} className="form-check">
-								<input
-									type="radio"
-									className="form-check-input"
-									name="answer"
-									id={`option${index}`}
-									value={option}
-								/>
-								<label className="form-check-label" htmlFor={`option${index}`}>
-									{option}
-								</label>
-							</div>
-						))}
-						<button type="submit" className="btn btn-warning mt-3 answerBtn" disabled={isFormDisabled}>
-							Submit
-						</button>
-					</form>
-				</div>
-			</div>
-			<div className="d-block mx-lg-auto img-fluid px-2">
-        <img className="answer object-fit-contain d-block justify-content-center" src="/images/js/start.jpg" alt="answer" style={{maxWidth: '15em', height: '100%'}}></img>
+
+      <div className="col-12 col-md-8 col-lg-6 justify-content-center challenge text-center py-3 px-4" style={{ border: '10px solid #0d213c', borderRadius: '8px', backgroundColor: 'rgba(35, 105, 173, 0.5)' }}>
+        <h3 className="question" style={{ fontWeight: '700', borderBottom: '4px solid rgba(255, 100, 0, 0.5)', marginBottom: '1rem' }}>{question}</h3>
+      	<form onSubmit={handleAnswerButton} disabled={isFormDisabled}>
+          <div className="btn-group-vertical w-100">
+            {answerOptions.map((option, index) => (
+            <div key={uuidv4()} className="form-check w-100">
+			      <input type="radio" сlassName="btn-check" name="answer" id={`option${index}`} value={option}/>
+			      <label className="btn btn-outline-primary" htmlFor={`option${index}`}>
+				      {option}
+			      </label>
+		        </div>
+            ))}
+          </div>
+         <button type="submit" className="btn btn-warning mt-3 answerBtn" disabled={isFormDisabled}>
+          Submit
+         </button>
+        </form>
       </div>
-		</div>
+   </div>
 	);
 }
